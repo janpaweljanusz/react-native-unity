c86e3f19eba274b10d3a5c481e394c5d0faf3f43
zbagley <zbagley@cgsinc.ca>
2019-08-15 09:14:07 -0500

    fix gitignore

---
+++ UnityProject/Assets/Plugins/iOS/NativeCallProxy.mm
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import "NativeCallProxy.h"
+
+
+@implementation FrameworkLibAPI
+
+id<NativeCallsProtocol> api = NULL;
++(void) registerAPIforNativeCalls:(id<NativeCallsProtocol>) aApi
+{
+    api = aApi;
+}
+
+@end
+
+
+extern "C" {
+    void unityMessage(const char* color) { return [api unityMessage:[NSString stringWithUTF8String:color]]; }
+}
+
